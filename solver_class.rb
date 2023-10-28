0f6147142657fc9df27e2a2b86206068096285d4
stino-x <107426247+stino-x@users.noreply.github.com>
2023-10-28 21:10:38 +0200

    Added a `fizzbuzz` method to `Solver` class.
    The method returns "fizz" when argument is divisible by 3, "buzz" when argument is divisible by 5, "fizzbuzz" when argument is divisible by 3 and 5, and the string of the number if none of the previous conditions are met.
    
    Also added unit tests for the `fizzbuzz` method.

--- solver_class.rb
+++ solver_class.rb
@@ -15,16 +15,28 @@ class Solver
 
   def reverse(string)
     raise ArgumentError, 'Input must be a string' unless string.is_a?(String)
-​
+
     array = string.chars
-​
+
     array_as_string = array
     length_of_array_string = array_as_string.length
     reversed_string_array = []
-​
+
     (length_of_array_string - 1).downto(0) do |index|
       reversed_string_array << array_as_string[index]
     end
     reversed_string_array.join
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
--- solver_class_spec.rb
+++ solver_class_spec.rb
@@ -16,12 +16,28 @@ describe 'Tests the Solver Class' do
   it 'returns 1 when the argument is 0' do
     expect(@object.factorial(0)).to eq(1)
   end
-  
+
   it 'tests if the reverse method raises an error for non-string input' do
     expect { @object.reverse(6) }.to raise_error(ArgumentError, 'Input must be a string')
   end
-​
+
   it 'tests if the reverse method reverses a string input' do
     expect(@object.reverse('Austin')).to eq('nitsuA')
+  end
+
+  it 'returns "fizz" when argument is divisible by 3' do
+    expect(@object.fizzbuzz(9)).to eq('fizz')
+  end
+
+  it 'returns "buzz" when argument is divisible by 5' do
+    expect(@object.fizzbuzz(10)).to eq('buzz')
+  end
+
+  it 'returns "fizzbuzz" when argument is divisible by 3 and 5' do
+    expect(@object.fizzbuzz(15)).to eq('fizzbuzz')
+  end
+
+  it 'returns the string of the number if none of the previous conditions are met' do
+    expect(@object.fizzbuzz(7)).to eq('7')
   end
 end
